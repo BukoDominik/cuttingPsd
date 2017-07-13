5bcf957067fc922c44f6c1b2d3434bf6cf91858d
Dominik <buko.dominik@gmail.com>
2017-07-13 14:27:03 +0200

    Added autoprefixer + minify to css file

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,16 @@
 var gulp = require("gulp"),
 	sass = require("gulp-sass"),
+	autoPrefixer = require("gulp-autoprefixer"),
+	cleanCss = require("gulp-clean-css"),
+	useref = require("gulp-useref"),
 	browserSync = require("browser-sync");
 
 	gulp.task("css", function(){
 		gulp.src("sass/style.scss")
 		.pipe(sass())
+		.pipe(autoPrefixer({
+            browsers: ["last 5 version", 'last 5 ff versions', 'IE 9',  'last 2 Chrome versions', 'last 5 Opera versions']
+        }))
 		.pipe(gulp.dest("css"))
 		.pipe(browserSync.stream());
 	});
@@ -20,4 +26,10 @@ var gulp = require("gulp"),
 		gulp.watch("*.html").on('change', browserSync.reload);
 	});
 
-	gulp.task("default", ["css", "server", "watch"]);
+	gulp.task("mini", function(){
+		gulp.src("css/style.css")
+			.pipe(cleanCss())
+			.pipe(gulp.dest("css/css"));
+	});
+
+	gulp.task("default", ["css", "mini", "server", "watch"]);
